5a770afd157092598875660b8d85fa303d0a779d
ferryrules <ferris@ferryrules.com>
2019-09-24 21:24:09 -0400

    semnatic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-import { Segment } from 'semantic-ui-react'
+import { Segment, Button } from 'semantic-ui-react'
 
 export default class App extends React.Component {
   render() {
     return(
       <div>
         <Segment raised>Sup bro</Segment>
+        <Button>Green</Button>
       </div>
     )
   }
