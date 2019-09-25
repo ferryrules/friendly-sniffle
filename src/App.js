7a5f514163ee15a9fe5d3da7927e7963c8927d8b
ferryrules <ferris@ferryrules.com>
2019-09-24 22:11:52 -0400

    semantic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import './App.css';
-import { Segment, Button } from 'semantic-ui-react'
+import { Segment, Button, Icon } from 'semantic-ui-react'
 
 export default class App extends React.Component {
   render() {
     return(
       <div>
         <Segment raised>Sup bro</Segment>
-        <Button>Green</Button>
+        <Button inverted color="red">Red</Button>
+        <Button animated color="yellow">
+          <Button.Content visible>Next</Button.Content>
+          <Button.Content hidden>
+            <Icon name='arrow right' />
+          </Button.Content>
+        </Button>
+        <Button animated='vertical' color="blue">
+          <Button.Content hidden>Shop</Button.Content>
+          <Button.Content visible>
+            <Icon name='shop' />
+          </Button.Content>
+        </Button>
+        <Button animated='fade' color="purple">
+          <Button.Content visible>Sign-up for a Pro account</Button.Content>
+          <Button.Content hidden>$12.99 a month</Button.Content>
+        </Button>
       </div>
     )
   }
