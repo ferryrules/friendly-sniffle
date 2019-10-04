d39248fa061a623f2ed00fe7bc5355ce36d3d484
ferryrules <ferris@ferryrules.com>
2019-10-04 15:25:48 -0400

    parallax

--- src/App.js
+++ src/App.js
@@ -1,39 +1,58 @@
 import React from 'react';
-import './App.css';
-// import { Segment, Icon } from 'semantic-ui-react'
-import { Button } from '@material-ui/core';
-import { Battery20, Battery30 } from '@material-ui/icons';
-import { Terminal as XTerm, type Theme } from 'xterm';
+import { Parallax, Background } from 'react-parallax';
 
-export default class App extends React.Component {
-  render() {
-    const terminal = new Terminal()
-    return(
-      <div>
-        <Button variant="contained" >
-          Default
-        </Button>
-        <Button variant="contained" href="#contained-buttons" >
-          Link
-        </Button>
-        <input
-          accept="image/*"
-          id="contained-button-file"
-          multiple
-          type="file"
-          hidden
-        />
-        <label htmlFor="contained-button-file">
-          <Button  component="span" >
-            Upload
-          </Button>
-        </label>
-        <br />
-        <br />
-        <Battery20 />
-        <Battery30 />
-        <Terminal />
-      </div>
-    )
-  }
-}
+const MyComponent = () => (
+    <div>
+        {/* -----basic config-----*/}
+        <Parallax
+            blur={10}
+            bgImage={require('path/to/image.jpg')}
+            bgImageAlt="the cat"
+            strength={200}
+        >
+            Put some text content here - even an empty div with fixed dimensions to have a height
+            for the parallax.
+            <div style={{ height: '200px' }} />
+        </Parallax>
+
+        {/* -----dynamic blur-----*/}
+        <Parallax
+            blur={{ min: -15, max: 15 }}
+            bgImage={require('path/to/another/image.jpg')}
+            bgImageAlt="the dog"
+            strength={-200}
+        >
+            Blur transition from min to max
+            <div style={{ height: '200px' }} />
+        </Parallax>
+
+        {/* -----custom background element-----*/}
+        <Parallax strength={300}>
+            <div>Use the background component for custom elements</div>
+            <Background className="custom-bg">
+                <img src="http://www.fillmurray.com/500/320" alt="fill murray" />
+            </Background>
+        </Parallax>
+
+        {/* -----renderProp: "renderLayer"-----*/}
+        <Parallax
+            bgImage={'/path/to/another/image'}
+            strength={400}
+            renderLayer={percentage => (
+                <div
+                    style={{
+                        position: 'absolute',
+                        background: `rgba(255, 125, 0, ${percentage * 1})`,
+                        left: '50%',
+                        top: '50%',
+                        width: percentage * 500,
+                        height: percentage * 500,
+                    }}
+                />
+            )}
+        >
+            <p>... Content</p>
+        </Parallax>
+    </div>
+);
+export default MyComponent;
