e7c77eb01904863de069638ab43f3ac1ae8b001a
ferryrules <ferris@ferryrules.com>
2019-10-04 15:33:47 -0400

    parallax sample

--- src/App.js
+++ src/App.js
@@ -1,58 +1,77 @@
-import React from 'react';
-import { Parallax, Background } from 'react-parallax';
+import React from "react";
+// import { render } from "react-dom";
+import { Parallax } from "react-parallax";
+import Hello from "./Hello";
 
-const MyComponent = () => (
-    <div>
-        {/* -----basic config-----*/}
-        <Parallax
-            blur={10}
-            bgImage={require('./logo192.png')}
-            bgImageAlt="the cat"
-            strength={200}
-        >
-            Put some text content here - even an empty div with fixed dimensions to have a height
-            for the parallax.
-            <div style={{ height: '1000px' }} />
-        </Parallax>
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center"
+};
+const insideStyles = {
+  background: "white",
+  padding: 20,
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%,-50%)"
+};
+const image1 =
+  "https://images.unsplash.com/photo-1498092651296-641e88c3b057?auto=format&fit=crop&w=1778&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D";
+const image2 =
+  "https://img00.deviantart.net/2bd0/i/2009/276/c/9/magic_forrest_wallpaper_by_goergen.jpg";
+const image3 =
+  "https://brightcove04pmdo-a.akamaihd.net/5104226627001/5104226627001_5297440765001_5280261645001-vs.jpg?pubId=5104226627001&videoId=5280261645001";
+const image4 =
+  "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/empire-state-building-black-and-white-square-format-john-farnan.jpg";
 
-        {/* -----dynamic blur-----*/}
-        <Parallax
-            blur={{ min: -15, max: 15 }}
-            bgImage={require('./logo512.png')}
-            bgImageAlt="the dog"
-            strength={-200}
-        >
-            Blur transition from min to max
-            <div style={{ height: '200px' }} />
-        </Parallax>
-
-        {/* -----custom background element-----*/}
-        <Parallax strength={300}>
-            <div>Use the background component for custom elements</div>
-            <Background className="custom-bg">
-                <img src="http://www.fillmurray.com/500/320" alt="fill murray" />
-            </Background>
-        </Parallax>
-
-        {/* -----renderProp: "renderLayer"-----*/}
-        <Parallax
-            bgImage={'/path/to/another/image'}
-            strength={400}
-            renderLayer={percentage => (
-                <div
-                    style={{
-                        position: 'absolute',
-                        background: `rgba(255, 125, 0, ${percentage * 1})`,
-                        left: '50%',
-                        top: '50%',
-                        width: percentage * 500,
-                        height: percentage * 500,
-                    }}
-                />
-            )}
-        >
-            <p>... Content</p>
-        </Parallax>
-    </div>
+const App = () => (
+  <div style={styles}>
+    <Hello name="Parallax" />
+    <Parallax bgImage={image1} strength={500}>
+      <div style={{ height: 500 }}>
+        <div style={insideStyles}>HTML inside the parallax</div>
+      </div>
+    </Parallax>
+    <h1>| | |</h1>
+    <Parallax bgImage={image3} blur={{ min: -1, max: 3 }}>
+      <div style={{ height: 500 }}>
+        <div style={insideStyles}>Dynamic Blur</div>
+      </div>
+    </Parallax>
+    <h1>| | |</h1>
+    <Parallax bgImage={image2} strength={-100}>
+      <div style={{ height: 500 }}>
+        <div style={insideStyles}>Reverse direction</div>
+      </div>
+    </Parallax>
+    <h1>| | |</h1>
+    <Parallax
+      bgImage={image4}
+      strength={200}
+      renderLayer={percentage => (
+        <div>
+          <div
+            style={{
+              position: "absolute",
+              background: `rgba(255, 125, 0, ${percentage * 1})`,
+              left: "50%",
+              top: "50%",
+              borderRadius: "50%",
+              transform: "translate(-50%,-50%)",
+              width: percentage * 500,
+              height: percentage * 500
+            }}
+          />
+        </div>
+      )}
+    >
+      <div style={{ height: 500 }}>
+        <div style={insideStyles}>renderProp</div>
+      </div>
+    </Parallax>
+    <div style={{ height: 500 }} />
+    <h2>{"\u2728"}</h2>
+  </div>
 );
-export default MyComponent;
+
+export default App
